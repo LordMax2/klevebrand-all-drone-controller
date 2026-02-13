e79df9b9fe52616825ac8e9ca62c08813ce00379
LordMax2 <max.klevebrand@gmail.com>
2026-02-13 16:41:01 +0100

    Cleanup gyro and motor and make them abstract classes instead of templates

---
+++ include/base_drone_motor.h
@@ -0,0 +1,10 @@
+#ifndef BASE_DRONE_MOTOR_H
+#define BASE_DRONE_MOTOR_H
+
+class BaseDroneMotor {
+public:
+    BaseDroneMotor() {};
+    virtual void setSpeed(float percentage);
+};
+
+#endif // BASE_DRONE_MOTOR_H
