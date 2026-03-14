6b28c4ec10ade8a626061fd461900b957e898d86
Max Klevebrand <max.klevebrand@gmail.com>
2026-03-14 21:46:35 +0100

    Make flight controller proxy virtual, new version

--- include/flight_controller_proxy.h
+++ include/flight_controller_proxy.h
@@ -3,16 +3,17 @@
 
 #include "base_drone.h"
 
-#define CONTROL_ACTION_COUNT 32
+#define CONTROL_ACTION_COUNT            16
 
-#define INCREASE_THROTTLE_CONTROL_ID 1
-#define DECREASE_THROTTLE_CONTROL_ID 2
-#define INCREASE_YAW_CONTROL_ID 3
-#define DECREASE_YAW_CONTROL_ID 4
-#define INCREASE_PITCH_CONTROL_ID 5
-#define DECREASE_PITCH_CONTROL_ID 6
-#define INCREASE_ROLL_CONTROL_ID 7
-#define DECREASE_ROLL_CONTROL_ID 8
+#define DO_NOTHING_CONTROL_ID           0
+#define INCREASE_THROTTLE_CONTROL_ID    1
+#define DECREASE_THROTTLE_CONTROL_ID    2
+#define INCREASE_YAW_CONTROL_ID         3
+#define DECREASE_YAW_CONTROL_ID         4
+#define INCREASE_PITCH_CONTROL_ID       5
+#define DECREASE_PITCH_CONTROL_ID       6
+#define INCREASE_ROLL_CONTROL_ID        7
+#define DECREASE_ROLL_CONTROL_ID        8
 
 typedef void (*ControlAction)(BaseDrone *drone);
 
@@ -24,13 +25,15 @@ typedef void (*ControlAction)(BaseDrone *drone);
  * And also, maybe it's nice with a class that is a nice and easy entrypoint to control a drone.
  */
 class FlightControllerProxy {
-private:
-    ControlAction _control_actions[CONTROL_ACTION_COUNT];
+protected:
+    ControlAction control_actions[CONTROL_ACTION_COUNT];
 
 public:
+    virtual ~FlightControllerProxy() = default;
+
     FlightControllerProxy();
 
-    void runAction(BaseDrone *drone, int id) const;
+    virtual void runAction(BaseDrone *drone, int id) const;
     void increaseThrottle(BaseDrone *drone) const;
     void decreaseThrottle(BaseDrone *drone) const;
     void increaseYaw(BaseDrone *drone) const;
--- src/flight_controller_proxy.cpp
+++ src/flight_controller_proxy.cpp
@@ -1,44 +1,44 @@
 #include "flight_controller_proxy.h"
 
-FlightControllerProxy::FlightControllerProxy() : _control_actions{}
+FlightControllerProxy::FlightControllerProxy() : control_actions{}
 {
-    _control_actions[0] = [](BaseDrone *drone) { /* Do nothing */ };
-    _control_actions[INCREASE_THROTTLE_CONTROL_ID] = [](BaseDrone *drone)
+    control_actions[DO_NOTHING_CONTROL_ID] = [](BaseDrone *drone) { /* Do nothing */ };
+    control_actions[INCREASE_THROTTLE_CONTROL_ID] = [](BaseDrone *drone)
     {
         drone->setThrottle(drone->getThrottle() + 1);
     };
-    _control_actions[DECREASE_THROTTLE_CONTROL_ID] = [](BaseDrone *drone)
+    control_actions[DECREASE_THROTTLE_CONTROL_ID] = [](BaseDrone *drone)
     {
         drone->setThrottle(drone->getThrottle() - 1);
     };
-    _control_actions[INCREASE_YAW_CONTROL_ID] = [](BaseDrone *drone)
+    control_actions[INCREASE_YAW_CONTROL_ID] = [](BaseDrone *drone)
     {
         drone->setDesiredYawAngle(drone->getDesiredYawAngle() + 1);
     };
-    _control_actions[DECREASE_YAW_CONTROL_ID] = [](BaseDrone *drone)
+    control_actions[DECREASE_YAW_CONTROL_ID] = [](BaseDrone *drone)
     {
         drone->setDesiredYawAngle(drone->getDesiredYawAngle() - 1);
     };
-    _control_actions[INCREASE_PITCH_CONTROL_ID] = [](BaseDrone *drone)
+    control_actions[INCREASE_PITCH_CONTROL_ID] = [](BaseDrone *drone)
     {
         drone->setDesiredPitchAngle(drone->getDesiredPitchAngle() + 1);
     };
-    _control_actions[DECREASE_PITCH_CONTROL_ID] = [](BaseDrone *drone)
+    control_actions[DECREASE_PITCH_CONTROL_ID] = [](BaseDrone *drone)
     {
         drone->setDesiredPitchAngle(drone->getDesiredPitchAngle() - 1);
     };
-    _control_actions[INCREASE_ROLL_CONTROL_ID] = [](BaseDrone *drone)
+    control_actions[INCREASE_ROLL_CONTROL_ID] = [](BaseDrone *drone)
     {
         drone->setDesiredRollAngle(drone->getDesiredRollAngle() + 1);
     };
-    _control_actions[DECREASE_ROLL_CONTROL_ID] = [](BaseDrone *drone)
+    control_actions[DECREASE_ROLL_CONTROL_ID] = [](BaseDrone *drone)
     {
         drone->setDesiredRollAngle(drone->getDesiredRollAngle() - 1);
     };
 }
 
 void FlightControllerProxy::runAction(BaseDrone *drone, const int id) const {
-    _control_actions[id](drone);
+    control_actions[id](drone);
 }
 
 void FlightControllerProxy::increaseThrottle(BaseDrone *drone) const {
